f5793d65af88b9b53b78f8c12f977e6587f2c703
bitjira <yogesh.talreja@i2p.com.pk>
2022-03-18 17:29:39 +0500

    Changed Menu from Single to List

--- src/main/java/com/example/mess/service/MenuService.java
+++ src/main/java/com/example/mess/service/MenuService.java
@@ -6,6 +6,9 @@ import com.example.mess.repository.MenuRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class MenuService {
@@ -13,8 +16,9 @@ public class MenuService {
     private final MenuRepository repository;
     private final MenuMapper mapper;
 
-   public Menu findByMessId(String messId){
-        return mapper.eToM(repository.findByMessId(messId));
+   public List<Menu> findByMessId(String messId){
+       return repository.findAllByMessId(messId)
+               .stream().map(mapper::eToM).collect(Collectors.toList());
     }
 
 }
